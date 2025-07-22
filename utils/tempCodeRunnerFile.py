4442d8093aa54bbd3de44264c30311b047744c7e
Tejas Pal <tejaspal0001@gmail.com>
2025-07-22 18:59:05 +0530

    minor changes done

--- utils/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
- # import matplotlib.pyplot as plt
-    # plt.imshow(X_img[0])
-    # plt.title(f"Class: {class_names[np.argmax(y_cat[0])]}")
-    # plt.show()
-
-    # plt.plot(X_ppg[0])
-    # plt.title("PPG Signal")
-    # plt.show()
